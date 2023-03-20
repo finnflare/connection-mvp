344956ffe60122b46166904dedac7a777d72f92c
finnflare <finnflare05@icloud.com>
2023-03-19 19:49:11 -0500

    Added 'roles' or paths for different types of users--staff and loved ones--which can't be accessed by the other roles, achieved through verification checking in _layout files

--- connection-mvp/app/loved-one-in/loved-one-home.js
+++ connection-mvp/app/loved-one-in/loved-one-home.js
@@ -1,12 +1,10 @@
 import { Link } from "expo-router";
 import { View, Text } from "react-native";
-import { useSearchParams } from "expo-router";
 
 const lovedOneHome = () => {
-  const { email } = useSearchParams();
   return (
     <View>
-      <Text>{email}</Text>
+      <Text>Loved One</Text>
     </View>
   );
 };
--- connection-mvp/app/staff-in/_layout.js
+++ connection-mvp/app/staff-in/_layout.js
@@ -4,12 +4,12 @@ import { auth, db } from "../../firebaseConfig";
 import { Slot, useRouter } from "expo-router";
 import { getDoc, doc } from "firebase/firestore";
 
-// This layout only allows authenticated users to access the loved-one-in folder
+// This layout only allows authenticated users to access the staff-in folder
 
 // This is listening for authentication
 // If user is authenticated, user is allowed to proceed in this directory
 // If user is unauthenticated, user is pushed back to index.js
-export default function lovedOneIn() {
+export default function staffIn() {
   const route = useRouter();
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -22,16 +22,16 @@ export default function lovedOneIn() {
     });
     return unsubscribe;
   }, []);
-  // loveV function to determine if user is a loved one
-  const loveV = async () => {
-    getDocument("loved-one", auth.currentUser.uid).catch((error) => {
+  // staffV function to determine if user is a staff
+  const staffV = async () => {
+    getDocument("staff", auth.currentUser.uid).catch((error) => {
       signOut(auth)
         .then(() => {})
         .catch((error) => {});
       route.push("");
     });
   };
-  // Returns a promise if user is a loved one, rejection else
+  // Returns a promise if user is a staff, rejection else
   async function getDocument(coll, id) {
     const snap = await getDoc(doc(db, coll, id));
     if (snap.exists()) {
@@ -40,8 +40,8 @@ export default function lovedOneIn() {
       return Promise.reject();
     }
   }
-  // Calling the loveV function, which checks verification of a loved one user
-  loveV();
+  // Calling the staffV function, which checks verification of a staff user
+  staffV();
   // If user is authenticated, give access to the directory
   return <Slot />;
 }
